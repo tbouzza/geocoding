44fa6be8b0f1d901441f80762c4641134705f6b9
Tarig Bouazzati <tbouzza@gmail.com>
2021-08-17 10:11:19 +0400

    added autocomplete on the new flat form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,10 +23,12 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import { initMapbox } from '../components/map';
+import { initMapbox } from '../components/init_mapbox';
+import { initAutocomplete } from '../components/init_autocomplete';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox()
+  initAutocomplete()
 });
